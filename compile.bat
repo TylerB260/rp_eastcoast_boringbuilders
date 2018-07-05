6a30991b0900bf68f731023b835ec828e437e107
DaaOtt <daaott@gmail.com>
2018-07-05 00:29:57 -0700

    updated compile script

--- compile.bat
+++ compile.bat
@@ -1,25 +1,2 @@
-@echo off
-if "%vproject%"=="" (
-	echo vproject not set! aborting
-	goto exit
-)
-set /a "compile_cores=%NUMBER_OF_PROCESSORS%-1"
-echo running with %compile_cores% cores
-@echo on
-
-"%vproject%\..\bin\vbsp.exe" -notjunc -game "%vproject%" "rp_eastcoast_boringbuilders"
-
-@if exist rp_eastcoast_boringbuilders.lin (
-@echo leak!!!!
-@goto exit
-)
-
-"%vproject%\..\bin\vvis.exe" -game "%vproject%" -threads %compile_cores% "rp_eastcoast_boringbuilders"
-
-"%vproject%\..\bin\vrad.exe" -game "%vproject%" -threads %compile_cores% "rp_eastcoast_boringbuilders"
-
-"%vproject%\..\..\Team Fortress 2\bin\bspzip.exe" -repack -compress "rp_eastcoast_boringbuilders.bsp"
-
-@echo off
-:exit
+lua compile.lua
 pause
---
+++ compile.lua
@@ -0,0 +1,50 @@
+local function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+local function execute(cmd)
+	--print("\n" .. cmd)
+	os.execute(cmd)
+end
+
+local vproject = os.getenv("vproject")
+if not vproject then
+	print("vproject not set! aborting")
+	os.exit()
+end
+
+local start_time = os.time()
+
+local num_cores = os.getenv("NUMBER_OF_PROCESSORS")
+print("machine has " .. num_cores .. " cores, running with " .. string.format("%i", num_cores - 1))
+num_cores = string.format("%i", num_cores - 1)
+
+execute(vproject .. [[\..\bin\vbsp.exe -notjunc -game "]] .. vproject .. [[" -threads ]] .. num_cores .. [[ "rp_eastcoast_boringbuilders"]])
+if file_exists("rp_eastcoast_boringbuilders.lin") then
+	print("leak!!!!!!!!")
+	os.exit()
+end
+
+execute(vproject .. [[\..\bin\vvis.exe -game "]] .. vproject .. [[" -threads ]] .. num_cores .. [[ "rp_eastcoast_boringbuilders"]])
+
+execute(vproject .. [[\..\bin\vrad.exe -game "]] .. vproject .. [[" -threads ]] .. num_cores .. [[ "rp_eastcoast_boringbuilders"]])
+
+local function recurse_and_add(local_path)
+	--print("local path", local_path)
+	for dir in io.popen([[dir "]] .. local_path .. [[" /b /ad]]):lines() do
+		--print("recursing dir", dir)
+		recurse_and_add(local_path .. dir .. [[\]])
+	end
+	for file in io.popen([[dir "]] .. local_path .. [[" /b /a-d]]):lines() do
+		--print("adding file", file)
+		local file_path = local_path .. file
+		execute([[""]] .. vproject .. [[\..\..\Team Fortress 2\bin\bspzip.exe" -addfile rp_eastcoast_boringbuilders.bsp "]] .. file_path .. [[" "]] .. file_path .. [[" rp_eastcoast_boringbuilders.bsp"]])
+	end
+end
+recurse_and_add([[materials\]])
+recurse_and_add([[models\]])
+execute([[""]] .. vproject .. [[\..\..\Team Fortress 2\bin\bspzip.exe" -repack -compress rp_eastcoast_boringbuilders.bsp"]])
+
+local seconds = os.time() - start_time
+print(string.format("\nProgram completed in %02d:%02d:%02d.", math.floor(seconds / 60 / 60), math.floor((seconds / 60) % 60), math.floor(seconds % 60)))
+os.exit()
